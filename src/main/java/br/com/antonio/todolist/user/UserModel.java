9d9965d4f093bd552a4b403b0316ccd8be520ac6
Antonio-pf <antoniopfelipe03@gmail.com>
2023-10-09 22:13:48 -0300

    Create Model-Controller user

---
+++ src/main/java/br/com/antonio/todolist/user/UserModel.java
@@ -0,0 +1,36 @@
+package br.com.antonio.todolist.user;
+
+public class UserModel {
+
+    private String name, username, password;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+      public String getUserName() {
+        return this.username;
+    }
+
+    public void setUserName(String username) {
+        this.username = username;
+    }
+
+
+      public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    
+    
+    
+}

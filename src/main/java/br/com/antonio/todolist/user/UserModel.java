1a60bb67a6521ff2b9fc2b28c2347b1e9262a58b
Antonio-pf <antoniopfelipe03@gmail.com>
2023-10-11 00:40:44 -0300

    Adicionado dependencias e persistindo os daddos de user

--- src/main/java/br/com/antonio/todolist/user/UserModel.java
+++ src/main/java/br/com/antonio/todolist/user/UserModel.java
@@ -1,36 +1,29 @@
 package br.com.antonio.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_users")
 public class UserModel {
 
-    private String name, username, password;
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
 
-    public String getName() {
-        return this.name;
-    }
+    @Column(unique = true)
+    private String name;
+    private String username;
+    private String password;
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-      public String getUserName() {
-        return this.username;
-    }
-
-    public void setUserName(String username) {
-        this.username = username;
-    }
-
-
-      public String getPassword() {
-        return this.password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-
-    
-    
-    
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }

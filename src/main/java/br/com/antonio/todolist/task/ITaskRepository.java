6214b84ccf5c52bc09c5b629cad110312d388e4a
Antonio-pf <antoniopfelipe03@gmail.com>
2023-10-14 12:02:59 -0300

    implementando seguranca nos dados do usuário

---
+++ src/main/java/br/com/antonio/todolist/task/ITaskRepository.java
@@ -0,0 +1,9 @@
+package br.com.antonio.todolist.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID>{
+    
+}
